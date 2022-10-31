4b0a357ec5bfcf9ea1acb39a50c04b05785d0d70
Shahista <shahista@veniteck.com>
2022-10-31 16:02:55 +0530

    Changes have been done in dashboard

--- newadmin/src/component/pages/Dashboard/Dashboard.jsx
+++ newadmin/src/component/pages/Dashboard/Dashboard.jsx
@@ -33,7 +33,7 @@ const Dasboard = () => {
           <div className="container-fluid">
             <div className="row mb-2">
               <div className="col-sm-6">
-                <h1 className="m-0 text-dark">Dashboard</h1>
+                <h1 className="m-0 text-dark">Dash</h1>
               </div>{/* /.col */}
               <div className="col-sm-6">
                 <ol className="breadcrumb float-sm-right">
